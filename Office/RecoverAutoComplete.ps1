d101d6eeebb1e5af76b851c4824457301d0ba3c7
proudcanadianeh <proudcanadianeh@shaw.ca>
2015-06-15 09:06:07 -0700

    Misc tools added.

---
+++ Office/RecoverAutoComplete.ps1
@@ -0,0 +1,10 @@
+﻿# %userprofile%\appdata\local\Microsoft\Outlook\RoamCache
+
+$largest = Get-ChildItem -Path $env:LOCALAPPDATA\Microsoft\Outlook\RoamCache -Filter "Stream_Autocomplete*" | Sort-Object -Property Length -Descending | select -First 1
+echo "$largest is largest"
+$newest = Get-ChildItem -Path $env:LOCALAPPDATA\Microsoft\Outlook\RoamCache -Filter "Stream_Autocomplete*" | Sort-Object -Property CreationTime -Descending |select -First 1
+echo "$newest is newest"
+Stop-Process -processname Outlook -Force -ErrorAction SilentlyContinue
+
+Rename-Item $env:LOCALAPPDATA\Microsoft\Outlook\RoamCache\$newest "backup_$newest"
+Rename-Item $env:LOCALAPPDATA\Microsoft\Outlook\RoamCache\$largest "$newest"
--- Windows/repartition drives.ps1
+++ Windows/repartition drives.ps1
@@ -1,4 +1,5 @@
 ﻿$getdisk = get-disk | ?{$_.DiskNumber -ne 0} |Out-GridView -PassThru
+if (!$getdisk){ exit }
 $detected = $null
 foreach ($disk in (Get-Partition -DiskNumber $getdisk.DiskNumber)){
 $drive = $disk.DriveLetter + ":\users"
@@ -12,9 +13,19 @@ if ($conf -ne 'y')
 {
 exit
 }
-Clear-Disk -Number $getdisk.DiskNumber -RemoveData -RemoveOEM -PassThru | 
+cls
+$newpart = Clear-Disk -Number $getdisk.DiskNumber -RemoveData -RemoveOEM -PassThru | 
 Initialize-Disk -PartitionStyle MBR -PassThru |
 New-Partition -UseMaximumSize -AssignDriveLetter | 
-Format-Volume
+Format-Volume -FileSystem NTFS
+$conf = Read-Host "Quick format of " $newpart.DriveLetter "complete. To benchmark speed press 1, for full format press 2, or anything else to exit."
+if ($conf -eq 1){
+echo "Benchmark"
+}elseif ($conf -eq 2){
+echo "Beginning format. Expect this to take a while"
+Format-Volume -DriveLetter $newpart.DriveLetter -FileSystem NTFS -Full -Force 
 
+}else{
+exit
+}
 #>
