b7719f98e4d6a763d53ae8cb7feb421ae205517d
proudcanadianeh <proudcanadianeh@gmail.com>
2021-05-11 22:29:23 -0700

    Chia update v3
    Added check for wallet, ability to set number of plots

--- chia.ps1
+++ chia.ps1
@@ -1,8 +1,8 @@
-﻿write-host "Welcome to Brians crappy Chia powershell script V2"
+﻿write-host "Welcome to Brians crappy Chia powershell script V3"
 
 #Running checks
 try {$version = Get-ChildItem $env:APPDATA\..\Local\chia-blockchain\ -Name app-* }catch{ echo "Version not detected";exit;}
-
+try {test-path $env:USERPROFILE\.chia\mainnet\config\ssl\wallet\private_wallet.crt}catch{ echo "Wallet not detected. Please configure";exit;}
 #end checks
 
 Write-Host "Detected drives:"
@@ -12,6 +12,9 @@ Write-Host "Enter TMP drive:"
 $tmpdrv = Read-Host
 write-host "Enter Final Drive:"
 $finaldrv = Read-Host
-$arguments = "plots create -k 32 -b 3389 -u 128 -r 2 -t $tmpdrv`:\Chia\tmp -d $finaldrv`:\Chia\Final -n 1"
+write-host "How many plots do you wish to make? (1)"
+$number = Read-Host
+if (!$number){$number = "1"}
+$arguments = "plots create -k 32 -b 3389 -u 128 -r 2 -t $tmpdrv`:\Chia\tmp -d $finaldrv`:\Chia\Final -n $number"
 write-host $arguments
 start-process -filepath $env:APPDATA\..\Local\chia-blockchain\$version\resources\app.asar.unpacked\daemon\chia.exe -ArgumentList $arguments
