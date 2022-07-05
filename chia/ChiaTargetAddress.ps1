1d7a3aeeb771c3e76f4fdfa79cf11c79374a4c4d
proudcanadianeh <bagland@langford.ca>
2022-07-05 16:42:29 -0700

    Re-Sync
    Re-sycning scripts repo

---
+++ chia/ChiaTargetAddress.ps1
@@ -0,0 +1,9 @@
+﻿write-host "Welcome to Brians crappy Chia powershell script for deteching your pool address V1"
+
+#Running checks
+try {$version = Get-ChildItem $env:APPDATA\..\Local\chia-blockchain\ -Name app-* }catch{ echo "Version not detected";exit;}
+try {test-path $env:USERPROFILE\.chia\mainnet\config\ssl\wallet\private_wallet.crt}catch{ echo "Wallet not detected. Please configure";exit;}
+#end checks
+
+$output = cmd /c $env:APPDATA\..\Local\chia-blockchain\$version\resources\app.asar.unpacked\daemon\chia.exe plotnft show 2`>`&1
+echo $output
