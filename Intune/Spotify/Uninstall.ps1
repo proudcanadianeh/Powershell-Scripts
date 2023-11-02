01ef9647b2e6fcf2c8742c1e62af76183df11479
proudcanadianeh <proudcanadianeh@gmail.com>
2023-11-02 15:17:24 -0700

    Update Uninstall.ps1

--- Intune/Spotify/Uninstall.ps1
+++ Intune/Spotify/Uninstall.ps1
@@ -1,3 +1,5 @@
-﻿$path = "C:\Program Files\Spotify\Spotify.exe"
+﻿Stop-Process Spotify -ErrorAction SilentlyContinue
+
+$path = "C:\Program Files\Spotify\Spotify.exe"
 $args = "/uninstall /silent"
 Start-Process $path $args -wait
